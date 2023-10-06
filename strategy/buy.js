9c68be87a1adf9d91f254c8a9ea844d85d79d2fb
ramseez <147175140+ramseez@users.noreply.github.com>
2023-10-06 17:11:32 +0100

    buy.js

--- strategy/buy.js
+++ strategy/buy.js
@@ -14,44 +14,43 @@ export const BuyStrategy = {
       operator: STRATEGY_OPERATORS.AND,
       conditions: [
         // 价格
-        { type: STRATEGY_TYPES.KEY_PRICE, value: 0.00008 },
+        { type: STRATEGY_TYPES.KEY_PRICE, value:  0.000071875 },
         // 推特关注数
-        { type: STRATEGY_TYPES.TWITTER_FOLLOWERS, value: 500 },
+        //{ type: STRATEGY_TYPES.TWITTER_FOLLOWERS, value: 500 },
         // 推特文章数
-        { type: STRATEGY_TYPES.TWITTER_POSTS, value: 20 },
+        //{ type: STRATEGY_TYPES.TWITTER_POSTS, value: 20 },
         // 推特平均阅读量(如果关注数设置得比较小，阅读量和 like 量建议设置为 0，因为这类蓝V可能获取不到阅读数)
-        { type: STRATEGY_TYPES.TWITTER_VIEWS, value: 0 },
+        //{ type: STRATEGY_TYPES.TWITTER_VIEWS, value: 0 },
         // 推特推特平均 Like 量
-        { type: STRATEGY_TYPES.TWITTER_FAVS, value: 0 },
+        //{ type: STRATEGY_TYPES.TWITTER_FAVS, value: 0 },
       ],
     },
     {
       operator: STRATEGY_OPERATORS.AND,
       conditions: [
         // 价格
-        { type: STRATEGY_TYPES.KEY_PRICE, value: 0.0002 },
+        { type: STRATEGY_TYPES.KEY_PRICE, value: 0.00008 },
         // 推特关注数
-        { type: STRATEGY_TYPES.TWITTER_FOLLOWERS, value: 1000 },
+        //{ type: STRATEGY_TYPES.TWITTER_FOLLOWERS, value: 1000 },
         // 推特文章数
-        { type: STRATEGY_TYPES.TWITTER_POSTS, value: 100 },
+        //{ type: STRATEGY_TYPES.TWITTER_POSTS, value: 100 },
         // 推特平均阅读量
-        { type: STRATEGY_TYPES.TWITTER_VIEWS, value: 500 },
+        //{ type: STRATEGY_TYPES.TWITTER_VIEWS, value: 500 },
         // 推特推特平均 Like 量
-        { type: STRATEGY_TYPES.TWITTER_FAVS, value: 30 },
+        //{ type: STRATEGY_TYPES.TWITTER_FAVS, value: 30 },
       ],
     },
     {
       // 白名单
       type: STRATEGY_TYPES.WHITELIST,
       whitelist: [
-        // { username: "zmzimpl", maxPrice: 0.0005, buyAmount: 1 },
-        // { username: "elonmusk", maxPrice: 0.05, buyAmount: 2 },
+        
       ],
     },
   ],
-  onlyBuyBlueVerified: true,
+  //onlyBuyBlueVerified: true,
   // 如果一个 key 是由 bots 列表内的地址出售的，不考虑买入
-  skipSoldByBot: true,
+  //skipSoldByBot: true,
   // 禁止多次买入同一个 share
   disabledMultiBuy: true,
 };
@@ -62,7 +61,7 @@ export const BOT_JUDGED_NONCE = 300;
 
 export const couldBeBought = ({ subject, trader, isBuy }, bots) => {
   const blockList = notBuyList.concat(bots);
-  const isInBlockList = blockList.some((address) => {
+/*  const isInBlockList = blockList.some((address) => {
     const isBlock = address.toLowerCase() === subject.toLowerCase();
     const isSoldByBot =
       BuyStrategy.skipSoldByBot &&
@@ -77,7 +76,7 @@ export const couldBeBought = ({ subject, trader, isBuy }, bots) => {
     }
     return isBlock || isSoldByBot;
   });
-
+*/
   let holdings = [];
   if (existsSync(getDir("holdings.json"))) {
     const rawData = readFileSync(getDir("holdings.json"), "utf-8");
@@ -90,16 +89,16 @@ export const couldBeBought = ({ subject, trader, isBuy }, bots) => {
   return !isInBlockList && !alreadyBuy;
 };
 
-const evaluateCondition = (condition, twitterInfo, shareInfo) => {
+const evaluateCondition = (condition, shareInfo) => {
   switch (condition.type) {
-    case STRATEGY_TYPES.TWITTER_VIEWS:
-      return twitterInfo.viewAvg >= condition.value;
-    case STRATEGY_TYPES.TWITTER_FAVS:
-      return twitterInfo.favoriteAvg >= condition.value;
-    case STRATEGY_TYPES.TWITTER_FOLLOWERS:
-      return twitterInfo.followers >= condition.value;
-    case STRATEGY_TYPES.TWITTER_POSTS:
-      return twitterInfo.posts >= condition.value;
+    //case STRATEGY_TYPES.TWITTER_VIEWS:
+      //return twitterInfo.viewAvg >= condition.value;
+    //case STRATEGY_TYPES.TWITTER_FAVS:
+      //return twitterInfo.favoriteAvg >= condition.value;
+    //case STRATEGY_TYPES.TWITTER_FOLLOWERS:
+      //return twitterInfo.followers >= condition.value;
+    //case STRATEGY_TYPES.TWITTER_POSTS:
+      //return twitterInfo.posts >= condition.value;
     case STRATEGY_TYPES.KEY_PRICE:
       return shareInfo.price < condition.value;
     case STRATEGY_TYPES.WHITELIST:
@@ -112,21 +111,21 @@ const evaluateCondition = (condition, twitterInfo, shareInfo) => {
   }
 };
 
-const evaluateStrategy = (strategy, twitterInfo, shareInfo) => {
+const evaluateStrategy = (strategy, shareInfo) => {
   if (strategy.operator) {
     if (strategy.operator === STRATEGY_OPERATORS.AND) {
       return strategy.conditions.every((condition) =>
-        evaluateStrategy(condition, twitterInfo, shareInfo)
+        evaluateStrategy(condition, shareInfo)
       );
     } else if (strategy.operator === STRATEGY_OPERATORS.OR) {
       return strategy.conditions.some((condition) =>
-        evaluateStrategy(condition, twitterInfo, shareInfo)
+        evaluateStrategy(condition, shareInfo)
       );
     } else {
       throw new Error("Unknown operator");
     }
   } else {
-    return evaluateCondition(strategy, twitterInfo, shareInfo);
+    return evaluateCondition(strategy, shareInfo);
   }
 };
 
@@ -164,12 +163,12 @@ export const isWhitelisted = (shareInfo) => {
   return user;
 };
 
-export const shouldFetchPrice = (twitterInfo, shareInfo) => {
-  return evaluateStrategy(BuyStrategy, twitterInfo, shareInfo);
+export const shouldFetchPrice = (shareInfo) => {
+  return evaluateStrategy(BuyStrategy, shareInfo);
 };
 
-export const shouldBuy = (twitterInfo, shareInfo) => {
-  return evaluateStrategy(BuyStrategy, twitterInfo, shareInfo);
+export const shouldBuy = (shareInfo) => {
+  return evaluateStrategy(BuyStrategy, shareInfo);
 };
 
 export const getMaxPrice = () => {
@@ -177,7 +176,7 @@ export const getMaxPrice = () => {
   return Math.max(...prices);
 };
 
-const containsTwitterConditions = (strategy) => {
+/*const containsTwitterConditions = (strategy) => {
   if (strategy.conditions) {
     for (let condition of strategy.conditions) {
       if (
@@ -193,8 +192,8 @@ const containsTwitterConditions = (strategy) => {
     }
   }
   return false;
-};
-const containsTwitterViewConditions = (strategy) => {
+};*/
+/*const containsTwitterViewConditions = (strategy) => {
   if (strategy.conditions) {
     for (let condition of strategy.conditions) {
       if (
@@ -210,12 +209,12 @@ const containsTwitterViewConditions = (strategy) => {
     }
   }
   return false;
-};
+};*/
 
-export const shouldFetchTwitterInfo = (accountInfo, shareInfo) => {
+/*export const shouldFetchTwitterInfo = (accountInfo, shareInfo) => {
   return containsTwitterConditions(BuyStrategy);
 };
 
 export const shouldFetchTwitterViewInfo = () => {
   return containsTwitterViewConditions(BuyStrategy);
-};
+};*/
